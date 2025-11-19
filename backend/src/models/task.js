646911fd554f4a6602eb6167d8102fd711d9cb02
Sankavi-Raveendran <sankaviraveendran@gmail.com>
2025-11-19 11:41:23 +0530

    updated

--- backend/src/models/task.js
+++ backend/src/models/task.js
@@ -1,10 +1,27 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../database.js";
 
-const Task = sequelize.define("Task", {
-  title: { type: DataTypes.STRING, allowNull: false },
-  description: { type: DataTypes.TEXT },
-  completed: { type: DataTypes.BOOLEAN, defaultValue: false }
-});
+const Task = sequelize.define(
+  "Task",
+  {
+    title: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT },
+    completed: { type: DataTypes.BOOLEAN, defaultValue: false },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW, 
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "Tasks",    
+    freezeTableName: true,  
+  }
+);
 
 export default Task;
